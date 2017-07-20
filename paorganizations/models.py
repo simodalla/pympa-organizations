05e5afcbae1999b67212f82963bd0404bceee5e7
Simone Dalla <simodalla@gmail.com>
2017-07-20 17:42:31 +0200

    new models

--- paorganizations/models.py
+++ paorganizations/models.py
@@ -4,13 +4,14 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from model_utils import Choices
 from model_utils.models import TimeStampedModel
 from pautils.models import Published
 
 
 class Organization(TimeStampedModel, Published):
     """Organizzazione"""
-    title = models.CharField(_('title'), max_length=500, unique=True)
+    title = models.CharField(_('title'), max_length=500)
     email_domain = models.CharField(_('email domain'), max_length=100,
                                     unique=True)
     parent = models.ForeignKey('self', blank=True, null=True,
@@ -20,6 +21,10 @@ class Organization(TimeStampedModel, Published):
     email_background_color = models.CharField(max_length=20,
                                               default='lightskyblue',
                                               blank=True)
+
+    class Meta:
+        verbose_name = _('organization')
+        verbose_name_plural = _('organizations')
 
     def __str__(self):
         return self.title
@@ -40,13 +45,39 @@ class Building(TimeStampedModel, Published):
         return '{}'.format(self.title)
 
 
+class Email(TimeStampedModel, Published):
+    address = models.EmailField(_('email address'), blank=True)
+
+    class Meta:
+        verbose_name = _('email')
+        verbose_name_plural = _('email')
+
+    def __str__(self):
+        return '{}'.format(self.address)
+
+
+class TelephoneNumber(TimeStampedModel, Published):
+    TYPES = Choices(('fix', _('fix')), ('internal', _('internal')),
+                    ('mobile', _('mobile')), ('fax', _('fax')),)
+    number = models.EmailField(_('number'), blank=True)
+    type = models.CharField(choices=TYPES, default=TYPES.fix,
+                              max_length=20)
+
+    class Meta:
+        verbose_name = _('telephone number')
+        verbose_name_plural = _('telephone number')
+
+    def __str__(self):
+        return '{}'.format(self.number)
+
+
 class Person(TimeStampedModel, Published):
     """Persona"""
-    owner = models.OneToOneField(settings.AUTH_USER_MODEL, blank=True,
-                                 null=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, blank=True, null=True)
     first_name = models.CharField(_('first name'), max_length=50, blank=True)
     last_name = models.CharField(_('last name'), max_length=50, blank=True)
-    email = models.EmailField(_('email address'), blank=True)
+    register_number = models.CharField(_('register number'), max_length=10,
+                                       blank=True)
 
     class Meta:
         verbose_name = _('person')
@@ -63,8 +94,6 @@ class Office(TimeStampedModel, Published):
                                      verbose_name=_('organization'))
     parent = models.ForeignKey('self', blank=True, null=True,
                                verbose_name=_('parent organization'))
-    components = models.ManyToManyField(Person, blank=True,
-                                        verbose_name=_('components'))
 
     class Meta:
         verbose_name = _('office')
@@ -72,5 +101,30 @@ class Office(TimeStampedModel, Published):
 
     def __str__(self):
         return '{}'.format(self.title)
+
+
+class Assignment(TimeStampedModel, Published):
+    """Assegnazione"""
+    organization = models.ForeignKey(Organization,
+                                     verbose_name=_('organization'))
+    person = models.ForeignKey(Person, verbose_name=_('person'))
+    email = models.ForeignKey(Email, blank=True,
+                              verbose_name=_('email'))
+    telephone_numbers = models.ManyToManyField(TelephoneNumber, blank=True,
+                                               verbose_name=_(
+                                                   'telephone numbers'))
+    notes = models.TextField(_('notes'), blank=True)
+    office = models.ForeignKey(Office, blank=True, verbose_name=_('office'))
+    buildings = models.ManyToManyField(Building, blank=True,
+                                       verbose_name=_('buildingss'))
+
+    class Meta:
+        verbose_name = _('assignment')
+        verbose_name_plural = _('assignments')
+
+    def __str__(self):
+        return '{} {}'.format(self.organization, self.person)
+
+
 
 
